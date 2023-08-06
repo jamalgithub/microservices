9df3a6be41d06e441311ce47214b7af6dba4208a
jamalgithub <jelmoubarik@gmail.com>
2023-08-06 14:29:17 +0200

    add Notification module

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,17 +2,18 @@ package com.amigoscode.customer;
 
 import com.amigoscode.clients.fraud.FraudCheckResponse;
 import com.amigoscode.clients.fraud.FraudClient;
+import com.amigoscode.clients.notification.NotificationClient;
+import com.amigoscode.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
     private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -30,5 +31,12 @@ public class CustomerService {
             throw new IllegalStateException("fraudster");
         }
         // todo: send notification
+        notificationClient.sendNotification(
+            new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome to Amigoscode...", customer.getFirstName())
+            )
+        );
     }
 }
